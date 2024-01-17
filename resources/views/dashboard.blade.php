ca17a1869bd5c09ed7b1179ca332e91d4d491dba
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2024-01-17 13:10:32 +0100

    test chang text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard Itzmine RUN DEV WORX !!! Apt fixed?! Watch realtime.') }}
+            {{ __('Dashboard Itzmine Happy New Year !!! 2024 will rock!') }}
         </h2>
     </x-slot>
 
